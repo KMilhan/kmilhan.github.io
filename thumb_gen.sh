7ac25a7da320acceedcce2d969f9303c151811c3
Milhan KIM <milhan.kim@woven-planet.global>
2023-05-04 14:15:26 +0900

    update

--- thumb_gen.sh
+++ thumb_gen.sh
@@ -1,4 +1,4 @@
 mkdir public/thumb -p
-rm -f public/thumb/*.png
-cp public/*.png public/thumb/
-mogrify -resize 1024x1024^ ./public/thumb/*.png
+rm -f public/thumb/*.png public/thumb/*.jpg
+cp public/*.png public/*.jpg public/thumb/
+mogrify -resize 1024x1024^ ./public/thumb/*.png ./public/thumb/*.jpg
